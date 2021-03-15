320d527fffaeef162741d9276b2ca525e3c2d5d2
chrislemus <icrislemus@gmail.com>
2021-03-15 11:21:31 -0400

    commit tree 59d921c81bcce47eacaf08db35f176ea254bfe43

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1', '#victory');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion"
